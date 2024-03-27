de8f041f83f406f59b02793d93584135a2660940
Roton02 <rph645102@gmail.com>
2024-03-27 07:19:20 +0600

    resolved sort by decending

--- src/Component/ReadBlog/ReadBlogs.jsx
+++ src/Component/ReadBlog/ReadBlogs.jsx
@@ -10,13 +10,13 @@ const ReadBlogs = () => {
     const [sortD, setSortD] = useState([...data]);
     const sortedData = (sort) => {
       if (sort === "Rating") {
-        const rating = data.sort((a, b) => a.rating - b.rating);
+        const rating = data.sort((a, b) => b.rating - a.rating);
         setSortD([...rating]);
       } else if (sort === "pages") {
-        const pages = data.sort((a, b) => a.totalPages - b.totalPages);
+        const pages = data.sort((a, b) => b.totalPages - a.totalPages);
         setSortD([...pages]);
       } else if (sort === "year") {
-        const year = data.sort((a, b) => a.yearOfPublishing - b.yearOfPublishing);
+        const year = data.sort((a, b) => b.yearOfPublishing - a.yearOfPublishing);
         setSortD([...year]);
       }
     };
